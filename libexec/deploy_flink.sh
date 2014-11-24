a1ae2356c5dabc88e58ce8cbf39e9ed6d3e52b8e
Till Rohrmann <trohrmann@apache.org>
2014-11-24 11:43:01 +0100

    Download own flink and job files

--- libexec/deploy_flink.sh
+++ libexec/deploy_flink.sh
@@ -1,3 +1,3 @@
-gsutil cp gs://flink-experiments/aljoscha/flink-yarn.tar.gz ./
+gsutil cp gs://flink-experiments/till/flink-yarn.tar.gz ./
 tar xzf flink-yarn.tar.gz
-gsutil cp gs://flink-experiments/aljoscha/flink-jobs-0.1-SNAPSHOT.jar ./
+gsutil cp gs://flink-experiments/till/flink-jobs-0.1-SNAPSHOT.jar ./
