935b1f02e291f94a7db5aadb45920786959fb87b
Till Rohrmann <trohrmann@apache.org>
2014-11-24 14:39:05 +0100

    updated flink binary path

--- libexec/deploy_flink.sh
+++ libexec/deploy_flink.sh
@@ -1,3 +1,3 @@
-gsutil cp gs://flink-experiments/till/flink-yarn.tar.gz ./
-tar xzf flink-yarn.tar.gz
+gsutil cp gs://flink-experiments/till/flink-yarn-0.8-incubating-SNAPSHOT.tar.gz ./
+tar xzf flink-yarn-0.8-incubating-SNAPSHOT.tar.gz
 gsutil cp gs://flink-experiments/till/flink-jobs-0.1-SNAPSHOT.jar ./
